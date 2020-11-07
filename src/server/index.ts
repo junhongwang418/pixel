503e80f48beb6c7f593257605f558741ad9ad5f8
ioneone <ionejunhong@gmail.com>
2020-11-07 10:01:50 -0800

    use dynamic port number

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static("dist/client"));
 
-app.listen(8080, () => {
-  console.log("Listening on port 8080");
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
